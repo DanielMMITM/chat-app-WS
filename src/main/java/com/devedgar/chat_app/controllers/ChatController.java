6540660e2e451c79e4a45038f0f06bbee96c6b82
Edgarm <edgar.martinez@canteradigital.mx>
2025-01-05 20:30:45 -0600

    Add - add websocket controller

--- src/main/java/com/devedgar/chat_app/controllers/ChatController.java
+++ src/main/java/com/devedgar/chat_app/controllers/ChatController.java
@@ -1,4 +1,19 @@
 package com.devedgar.chat_app.controllers;
 
+import com.devedgar.chat_app.models.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import java.util.Date;
+
+@Controller
 public class ChatController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
+        chatMessage.setTimestamp(new Date());
+        return chatMessage;
+    }
 }
