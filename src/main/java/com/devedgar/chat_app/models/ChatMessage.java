0b67a2a00bccd8f6974060ca4ccfe2f07bf27af5
Edgarm <edgar.martinez@canteradigital.mx>
2025-01-05 20:29:24 -0600

    Add - add chat message model

--- src/main/java/com/devedgar/chat_app/models/ChatMessage.java
+++ src/main/java/com/devedgar/chat_app/models/ChatMessage.java
@@ -1,4 +1,17 @@
 package com.devedgar.chat_app.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ToString
 public class ChatMessage {
+    private String nickname;
+    private String content;
+    private Date timestamp;
 }
