4193bf4cba59825f360b0aa90f89b6d1aec3b74d
Edgarm <edgar.martinez@canteradigital.mx>
2025-01-12 02:12:22 -0600

    Add - add health controller and endpoint

--- src/main/java/com/devedgar/chat_app/controllers/HealthController.java
+++ src/main/java/com/devedgar/chat_app/controllers/HealthController.java
@@ -1,4 +1,12 @@
 package com.devedgar.chat_app.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
+    @RequestMapping("/health")
+    public String health(){
+        return "Healthy!";
+    }
 }
